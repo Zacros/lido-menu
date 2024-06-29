2be0ce6fff8d8140bbe032f7ea0d8347488d26f4
Zacros <zacrostxt@gmail.com>
2024-06-29 16:07:35 +0200

    some styling and listings

--- generate-html-from-csv.py
+++ generate-html-from-csv.py
@@ -22,16 +22,16 @@ def generate_html(csv_file, output_file):
         html_content.append(f'<!--\n\nINIZIO SEZIONE: {section}\n\n-->')
 
         section_id = section.lower().replace(" ", "-")
-        html_content.append(f'<section id="{section_id}" class="mb-8">')
-        html_content.append('<div class="flex gap-2  items-end mb-6 ">')
-        html_content.append(f'   <img src="icons/{section}.png" alt="Caffetterie" class="w-8 h-8 object-contain " />')
+        html_content.append(f'<section id="{section_id}" class="mb-16">')
+        html_content.append('<div class="flex gap-2  items-end mb-10 ">')
+        html_content.append(f'   <img src="icons/{section}.png" alt="Caffetterie" class="w-12 h-12 object-contain " />')
         html_content.append(f'  <h2 class="text-lg mx-2 font-bold font-serif tracking-widest uppercase    bg-sky-1200  w-full border-double  shadow-sky-400 border-b border-sky-300 pb-1 px-2">{section}</h2>')
         html_content.append('</div>')
 
         html_content.append('  <ul class="space-y-1 px-4 text-sm">')
         for item, price in items:
             html_content.append(f'    <li class="flex justify-between border-b border-gray-200 border-dashed py-1">')
-            html_content.append(f'      <span>{item}</span><span>{price} €</span>')
+            html_content.append(f'      <span class="max-w-[80%]">{item}</span><span>{price} €</span>')
             html_content.append(f'    </li>')
         html_content.append('  </ul>')
         html_content.append('</section>')
